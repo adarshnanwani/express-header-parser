7ec945ce0fd8093f59e607cc487b0a1625a671ab
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-18 14:27:26 +0530

    Add a server.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    optionsSuccessStatus: 200
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.log("> Server is listening on PORT " + PORT);
+});
